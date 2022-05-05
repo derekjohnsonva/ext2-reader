f31824f45853c05d2c6e63f29773dcdc37694847
derekjohnsonva <derekjohnsonva@gmail.com>
2022-05-05 14:27:53 -0400

    add testing script

---
+++ test.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+#
+# functionality check script for Project 4A
+
+# compile the executable `p4exp1`
+make
+# Run the executable `p4exp1` with the first argument as the input file
+# pipe the output to a file `test.csv`
+echo $1
+echo $2
+./p4exp1 $1 > test.csv
+# if the second argument is not a file, exit with an error
+if [ ! -f $2 ]; then
+    echo "Input file $2 does not exist"
+    rm test.csv
+    exit 1
+fi
+
+# compare the output `test.csv` with the second argument
+diff <(sort $2) <(sort test.csv) > diff.txt
+# remove the temporary file
+rm test.csv
